47dcd3939db3109ec9537affa383024a4f3aef86
Monikamuruges <163965625+Monikamuruges@users.noreply.github.com>
2025-08-29 14:34:29 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,8 @@ function Contact() {
             <div class="footer">
         <h1 class="heading">Contact</h1>
        
-                <p>Email: monikam@gmail.com</p>
-                <p>Phone: 8428585477</p>
+                <p>Email: monikam102004@gmail.com</p>
+                <p>Phone: 8428858656</p>
                 <p>Connect with me <a href="https://www.linkedin.com/in/monika-murugesan-362b80259?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">LinkedIn</a></p>
             </div>
 </section>
