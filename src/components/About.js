55c2a03824be58f50f1edaa2ee1740b9f5e80420
Monikamuruges <163965625+Monikamuruges@users.noreply.github.com>
2025-08-29 14:25:00 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
             <h3>About Me</h3>
             <img src={profileImage} alt="Profile" className="profile-image" />
             <h2>Hello! I'm Monika M,Iam a Passionate Frontend Develeoper.</h2>
-                <p>I'm B.tech IT third-year student with strong passion for web design,particularly
+                <p>I'm B.tech IT fourth-year student with strong passion for web design,particularly
             in front-end development.Alongside my enthusiasm for creating Users interfaces,
             I have acquired foundational knowledge in programming languages such a Java,C,Python.
             Iam a fast learner with keen interest in continually expanding my technical skills
